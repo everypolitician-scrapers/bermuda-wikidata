403f30f862dabf7a6d8acb5f0068ecd5e28e048d
Tony Bowden <tony@tmtm.com>
2017-01-16 13:38:14 +0000

    Also scrape Premiers and Speakers

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,8 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Category:Members of the House of Assembly of Bermuda', 'en').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+members = WikiData::Category.new('Category:Members of the House of Assembly of Bermuda', 'en').member_titles
+premiers = WikiData::Category.new('Category:Premiers of Bermuda', 'en').member_titles
+speakers = WikiData::Category.new('Category:Speakers of the House of Assembly of Bermuda', 'en').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: members | premiers | speakers })
